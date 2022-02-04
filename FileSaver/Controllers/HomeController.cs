f41d8287cb6fee6a327a8f2fb6a60e59083e0e33
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2022-02-04 12:25:38 +0330

    feat: Added Save action to home contorller

--- FileSaver/Controllers/HomeController.cs
+++ FileSaver/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿using FileSaver.Models;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace FileSaver.Controllers
@@ -12,16 +14,31 @@ namespace FileSaver.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _env;
+        private string storagePath = "";
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IWebHostEnvironment env)
         {
             _logger = logger;
+            _env = env;
+            storagePath = $"{_env.ContentRootPath}\\Sotrage";
         }
-
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult Save(string title ,string body)
+        {
+            if (!Directory.Exists(storagePath))
+                Directory.CreateDirectory(storagePath);
 
+            using (FileStream fs = System.IO.File.Create(storagePath + $"\\{title}.txt"))
+            {
+                var content = new UTF8Encoding(true).GetBytes(body);
+                fs.Write(content, 0, content.Length);
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
